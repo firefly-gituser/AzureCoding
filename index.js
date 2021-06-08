c33b42165ed07f2d06f208d6685b63aee3372b40
firefly-gituser <65105006+firefly-gituser@users.noreply.github.com>
2021-06-08 22:56:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app =  express()
+const http = require('http');
 
-app.listen(3000,()=>console.log("Running....!"));
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
