6b47fca0c5b6770f0b2ab336ce639636bd7505c4
firefly-gituser <65105006+firefly-gituser@users.noreply.github.com>
2021-08-16 20:47:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,59 +21,64 @@ const config = {
     endpoint: "https://sb-openapi.zalopay.vn/v2/create"
 };
 
-const getzallopaylink = async() => {
-    const embed_data = {};
-    const items = [{}];
-    const transID = Math.floor(Math.random() * 1000000);
+const getzallopaylink = async({_username='',_items=[{}],_amount='0',_embed_data={}, _description='Payment Zalo App' } ) => {
+    const embed_data = _embed_data
+
+    const items = _items
+
+    const transID = Math.floor(Math.random() * 1000000) //  auto gen
+    
+    const description = _description
+
     const order = {
-        app_id: config.app_id,
-        app_trans_id: `${moment().format('YYMMDD')}_${transID}`, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
-        app_user: "user123",
-        app_time: Date.now(), // miliseconds
+        app_id: config.app_id, // not change
+        app_trans_id: `${moment().format('YYMMDD')}_${transID}`, // auto gen
+        app_user: _username,
+        app_time: Date.now(), // miliseconds auto gen the current time
         item: JSON.stringify(items),
         embed_data: JSON.stringify(embed_data),
-        amount: 50000,
-        description: `Lazada - Payment for the order #${transID}`,
+        amount: _amount,
+        description: `${description} #${transID}`,
         bank_code: "",
-    };
+    }
 
     // appid|app_trans_id|appuser|amount|apptime|embeddata|item
-    const data = config.app_id + "|" + order.app_trans_id + "|" + order.app_user + "|" + order.amount + "|" + order.app_time + "|" + order.embed_data + "|" + order.item;
-    order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+    const data = `${config.app_id}|${order.app_trans_id}|${order.app_user}|${order.amount}|${order.app_time}|${order.embed_data}|${order.item}`
+    
+    order.mac = CryptoJS.HmacSHA256(data, config.key1).toString()
 
     let axiosData = await axios.post(config.endpoint, null, { params: order })
+
     let respone = await axiosData.data
+    
     return respone.order_url
 }
 
 app.listen(port,()=>console.log('running'))
 
 app.get('/getlink', async (req, res) => {
-    console.log('call get link')
+
     let link = await getzallopaylink()
+
     res.status(200).json({ link })
 })
 
 app.post('/callback', (req, res) => {
     let result = {};
     try {
-        let dataStr = req.body.data;
-        let reqMac = req.body.mac;
+
+        let {dataStr, reqMac} = req.body
 
         let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
-        console.log("mac =", mac);
-        // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+
         if (reqMac !== mac) {
-            // callback không hợp lệ
             result.return_code = -1;
             result.return_message = "mac not equal";
         }
         else {
-            // thanh toán thành công
-            // merchant cập nhật trạng thái cho đơn hàng
             let dataJson = JSON.parse(dataStr, config.key2);
             console.log("update order's status = success where app_trans_id =", dataJson["app_trans_id"]);
-
+            //Update data to database
             result.return_code = 1;
             result.return_message = "success";
         }
@@ -81,13 +86,16 @@ app.post('/callback', (req, res) => {
         result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
         result.return_message = ex.message;
     }
-    // thông báo kết quả cho ZaloPay server
+
     res.json(result);
 });
 
 app.get('/complete',(req,res)=>{
+    console.log('this is params')
+    console.log(req.params)
     res.send('<h1>Transaction complete</h1>')
 })
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/page.html')
 })
